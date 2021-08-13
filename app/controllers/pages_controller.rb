c217acd6d9829fcc09f1773f623c3e42c8bfa037
Tiago de Castro <tiagodecastro.sf@gmail.com>
2021-08-13 10:42:39 +0100

    routes and pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,17 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def releases
+  end
+
+  def dates
+  end
+
+  def mixes
+  end
+
+  def contacts
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get '/releases' => 'pages#releases'
+  get '/dates' => 'pages#dates'
+  get '/mixes' => 'pages#mixes'
+  get '/contacts' => 'pages#contacts'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
