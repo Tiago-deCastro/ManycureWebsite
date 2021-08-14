5eb2b465fa161d37a0eeb38607dee2357ffb6193
Tiago de Castro <tiagodecastro.sf@gmail.com>
2021-08-14 10:07:48 +0100

    homepage js animation

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get '/dates' => 'pages#dates'
   get '/mixes' => 'pages#mixes'
   get '/contacts' => 'pages#contacts'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
